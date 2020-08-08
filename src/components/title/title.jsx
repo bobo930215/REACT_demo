895672c23c1f5fe81c4f5ae8375b496d26890ab2
BOBO <598527675@qq.com>
2020-08-08 12:01:55 +0800

    REACT demo

---
+++ src/components/title/title.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import './title.css'
+class Title extends Component {
+	render() {
+		return(
+			<header>
+				<h1>请发表对React的评论</h1>
+			</header>
+		)
+	}
+}
+export default Title
